354495dca163c13dc6ce50d0310e458b19f097a4
Andy Cdo <chela355@hotmail.com>
2021-12-09 16:37:05 -0500

    frontend and backehnd of sales

--- src/Services/sales.js
+++ src/Services/sales.js
@@ -4,18 +4,10 @@ export const create = (data) => {
   return api.post("/sales", data);
 };
 
-export const read = (id) => {
-  return api.get(`/sales/${id}`);
-};
-
 export const readAll = () => {
   return api.get("/sales");
 };
 
-export const update = (id, data) => {
-  return api.put(`/sales/${id}`, data);
-};
-
-export const remove = (id) => {
-  return api.delete(`/sales/${id}`);
+export const getLast = () => {
+  return api.get(`/sales/last`);
 };
--- src/Views/Sales/index.js
+++ src/Views/Sales/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ImageBackground from "../../Assets/background.jpg";
 import {
   Navbar,
@@ -12,6 +12,12 @@ import {
 import HomeIcon from "@mui/icons-material/Home";
 import LogoutIcon from "@mui/icons-material/Logout";
 import { Snackbar, Alert } from "@mui/material";
+import { read } from "../../Services/customers";
+import { read as readProduct } from "../../Services/products";
+import {
+  create as createSale,
+  getLast as lastSale,
+} from "../../Services/sales";
 
 const Sales = () => {
   const [loading, setLoading] = useState(false);
@@ -20,6 +26,177 @@ const Sales = () => {
     severity: "error",
     message: "",
   });
+  const [last, setLast] = useState(null);
+  const [id, setId] = useState("");
+  const [customer, setCostumer] = useState("");
+  const [consecutive, setconsecutive] = useState("");
+  const [productOne, setProductOne] = useState("");
+  const [nameProduct, setNameProduct] = useState("");
+  const [amount, setAmount] = useState("");
+  const [total, setTotal] = useState("");
+  const [product2, setProduct2] = useState("");
+  const [nameProduct2, setNameProduct2] = useState("");
+  const [amount2, setAmount2] = useState("");
+  const [total2, setTotal2] = useState("");
+  const [product3, setProduct3] = useState("");
+  const [nameProduct3, setNameProduct3] = useState("");
+  const [amount3, setAmount3] = useState("");
+  const [total3, setTotal3] = useState("");
+  const [totalSale, setTotalSale] = useState("");
+  const [totalIva, setTotalIva] = useState("");
+  const [sales, setSales] = useState("");
+  const [productPriceOne, setProductPriceOne] = useState("");
+  const [productPriceTwo, setProductPriceTwo] = useState("");
+  const [productPriceThree, setProductPriceThree] = useState("");
+  const [iva, setIva] = useState("");
+  const [iva2, setIva2] = useState("");
+  const [iva3, setIva3] = useState("");
+
+  useEffect(() => {
+    const getLast = async () => {
+      if (last === null) {
+        const result = await lastSale();
+        setLast(result.ok);
+        if (result.ok) {
+          setconsecutive(parseInt(result.data.code) + 1);
+        }
+      }
+    };
+    getLast();
+  }, [last]);
+
+  const saveSale = async () => {
+    try {
+      setLoading(true);
+      const data = {
+        code: consecutive,
+        identification: id,
+        details: [
+          {
+            amount: parseInt(amount),
+            code: productOne,
+            total: total,
+            value: total * iva + total,
+            iva: total * iva,
+          },
+          {
+            amount: parseInt(amount2),
+            code: product2,
+            total: total2,
+            value: total2 * iva2 + total2,
+            iva: total2 * iva2,
+          },
+          {
+            amount: parseInt(amount3),
+            code: product3,
+            total: total3,
+            value: total3 * iva3 + total3,
+            iva: total3 * iva3,
+          },
+        ],
+        total: sales,
+        value: totalSale,
+      };
+      const result = await createSale(data);
+      if (result.ok) {
+        setAlert({
+          ...alert,
+          severity: "success",
+          message: "Su compra se ha creado exitosamente",
+          open: true,
+        });
+      } else {
+        setAlert({
+          ...alert,
+          severity: "error",
+          message: "No se pudo confirmar la compra",
+          open: true,
+        });
+      }
+    } catch {
+      setAlert({
+        ...alert,
+        severity: "error",
+        message: "No se pudo confirmar la compra",
+        open: true,
+      });
+    }
+    setLoading(false);
+  };
+
+  const getCostumer = async () => {
+    try {
+      setLoading(true);
+      if (id && id !== "") {
+        const result = await read(id);
+        if (result.ok) {
+          setCostumer(result.data.name);
+        } else {
+          setAlert({
+            ...alert,
+            severity: "error",
+            message: "No se pudo consultar el cliente",
+            open: true,
+          });
+        }
+      }
+    } catch {
+      setAlert({
+        ...alert,
+        severity: "error",
+        message: "No se pudo leer el cliente",
+        open: true,
+      });
+    }
+    setLoading(false);
+  };
+
+  const getProduct = async (index) => {
+    try {
+      setLoading(true);
+      let productCode = "";
+      if (index === 1) {
+        productCode = productOne;
+      } else if (index === 2) {
+        productCode = product2;
+      } else if (index === 3) {
+        productCode = product3;
+      }
+      if (productCode && productCode !== "") {
+        const result = await readProduct(productCode);
+        if (result.ok) {
+          if (index === 1) {
+            setNameProduct(result.data.name);
+            setProductPriceOne(result.data.salePrice);
+            setIva(result.data.iva);
+          } else if (index === 2) {
+            setNameProduct2(result.data.name);
+            setProductPriceTwo(result.data.salePrice);
+            setIva2(result.data.iva);
+          } else if (index === 3) {
+            setNameProduct3(result.data.name);
+            setProductPriceThree(result.data.salePrice);
+            setIva3(result.data.iva);
+          }
+        } else {
+          setAlert({
+            ...alert,
+            severity: "error",
+            message: "No se pudo consultar el Producto",
+            open: true,
+          });
+        }
+      }
+    } catch {
+      setAlert({
+        ...alert,
+        severity: "error",
+        message: "No se pudo leer el Producto",
+        open: true,
+      });
+    }
+    setLoading(false);
+  };
 
   return (
     <>
@@ -64,24 +241,51 @@ const Sales = () => {
           <Col sm="3">
             <Form.Group className="mb-3 w-100">
               <Form.Label>Cédula</Form.Label>
-              <Form.Control type="text" />
+              <Form.Control
+                type="text"
+                onChange={(e) => {
+                  setId(e.target.value);
+                }}
+                value={id}
+              />
             </Form.Group>
           </Col>
           <Col sm="2">
-            <Button className="w-100" style={{ marginTop: 30 }} type="text">
+            <Button
+              className="w-100"
+              style={{ marginTop: 30 }}
+              type="text"
+              onClick={() => {
+                getCostumer();
+              }}
+            >
               Consultar
             </Button>
           </Col>
           <Col sm="4">
             <Form.Group className="mb-3 w-100">
               <Form.Label>Cliente</Form.Label>
-              <Form.Control type="text" />
+              <Form.Control
+                disabled
+                type="text"
+                onChange={(e) => {
+                  setCostumer(e.target.value);
+                }}
+                value={customer}
+              />
             </Form.Group>
           </Col>
           <Col sm="3">
             <Form.Group className="mb-3 w-100">
               <Form.Label>Consecutivo</Form.Label>
-              <Form.Control type="text" />
+              <Form.Control
+                disabled
+                type="text"
+                onChange={(e) => {
+                  setconsecutive(e.target.value);
+                }}
+                value={consecutive}
+              />
             </Form.Group>
           </Col>
         </Row>
@@ -89,84 +293,223 @@ const Sales = () => {
           <Col>
             <Form.Group className="mb-3 w-100">
               <Form.Label>Cod. Producto</Form.Label>
-              <Form.Control type="text" />
+              <Form.Control
+                type="text"
+                onChange={(e) => {
+                  setProductOne(e.target.value);
+                }}
+                value={productOne}
+              />
             </Form.Group>
           </Col>
           <Col>
-            <Button className="w-100" style={{ marginTop: 30 }} type="text">
+            <Button
+              className="w-100"
+              style={{ marginTop: 30 }}
+              type="text"
+              onClick={() => {
+                getProduct(1);
+              }}
+            >
               Consultar
             </Button>
           </Col>
           <Col>
             <Form.Group className="mb-3 w-100">
               <Form.Label>Nombre Producto</Form.Label>
-              <Form.Control type="text" />
+              <Form.Control
+                disabled
+                type="text"
+                onChange={(e) => {
+                  setNameProduct(e.target.value);
+                }}
+                value={nameProduct}
+              />
             </Form.Group>
           </Col>
           <Col>
             <Form.Group className="mb-3 w-100">
               <Form.Label>Cantidad</Form.Label>
-              <Form.Control type="text" />
+              <Form.Control
+                type="text"
+                onChange={(e) => {
+                  const currentTotal = e.target.value * productPriceOne;
+                  setAmount(e.target.value);
+                  setTotal(currentTotal);
+                  const saleTotal = currentTotal + total2 + total3;
+                  setTotalSale(saleTotal);
+
+                  const priceTotalIva1 = currentTotal * iva;
+                  const priceTotalIva2 = total2 * iva2;
+                  const priceTotalIva3 = total3 * iva3;
+                  const totalIva =
+                    priceTotalIva1 + priceTotalIva2 + priceTotalIva3;
+                  setTotalIva(totalIva);
+
+                  const sumaIva = parseInt(saleTotal) + parseInt(totalIva);
+                  setSales(sumaIva);
+                }}
+                value={amount}
+              />
             </Form.Group>
           </Col>
           <Col>
             <Form.Group className="mb-3 w-100">
               <Form.Label>Valor Total</Form.Label>
-              <Form.Control type="text" />
+              <Form.Control
+                disabled
+                type="text"
+                onChange={(e) => {
+                  setTotal(e.target.value);
+                }}
+                value={total}
+              />
             </Form.Group>
           </Col>
         </Row>
         <Row>
           <Col>
             <Form.Group className="mb-3 w-100">
-              <Form.Control type="text" />
+              <Form.Control
+                type="text"
+                onChange={(e) => {
+                  setProduct2(e.target.value);
+                }}
+                value={product2}
+              />
             </Form.Group>
           </Col>
           <Col>
-            <Button className="w-100" type="text">
+            <Button
+              className="w-100"
+              type="text"
+              onClick={() => {
+                getProduct(2);
+              }}
+            >
               Consultar
             </Button>
           </Col>
           <Col>
             <Form.Group className="mb-3 w-100">
-              <Form.Control type="text" />
+              <Form.Control
+                disabled
+                type="text"
+                onChange={(e) => {
+                  setNameProduct2(e.target.value);
+                }}
+                value={nameProduct2}
+              />
             </Form.Group>
           </Col>
           <Col>
             <Form.Group className="mb-3 w-100">
-              <Form.Control type="text" />
+              <Form.Control
+                type="text"
+                onChange={(e) => {
+                  const currentTotal = e.target.value * productPriceTwo;
+                  setAmount2(e.target.value);
+                  setTotal2(currentTotal);
+                  const saleTotal = currentTotal + total + total3;
+                  setTotalSale(saleTotal);
+
+                  const priceTotalIva1 = total * iva;
+                  const priceTotalIva2 = currentTotal * iva2;
+                  const priceTotalIva3 = total3 * iva3;
+                  const totalIva =
+                    priceTotalIva1 + priceTotalIva2 + priceTotalIva3;
+                  setTotalIva(totalIva);
+
+                  const sumaIva = parseInt(saleTotal) + parseInt(totalIva);
+                  setSales(sumaIva);
+                }}
+                value={amount2}
+              />
             </Form.Group>
           </Col>
           <Col>
             <Form.Group className="mb-3 w-100">
-              <Form.Control type="text" />
+              <Form.Control
+                disabled
+                type="text"
+                onChange={(e) => {
+                  setTotal2(e.target.value);
+                }}
+                value={total2}
+              />
             </Form.Group>
           </Col>
         </Row>
         <Row>
           <Col>
             <Form.Group className="mb-3 w-100">
-              <Form.Control type="text" />
+              <Form.Control
+                type="text"
+                onChange={(e) => {
+                  setProduct3(e.target.value);
+                }}
+                value={product3}
+              />
             </Form.Group>
           </Col>
           <Col>
-            <Button className="w-100" type="text">
+            <Button
+              className="w-100"
+              type="text"
+              onClick={() => {
+                getProduct(3);
+              }}
+            >
               Consultar
             </Button>
           </Col>
           <Col>
             <Form.Group className="mb-3 w-100">
-              <Form.Control type="text" />
+              <Form.Control
+                disabled
+                type="text"
+                onChange={(e) => {
+                  setNameProduct3(e.target.value);
+                }}
+                value={nameProduct3}
+              />
             </Form.Group>
           </Col>
           <Col>
             <Form.Group className="mb-3 w-100">
-              <Form.Control type="text" />
+              <Form.Control
+                type="text"
+                onChange={(e) => {
+                  const currentTotal = e.target.value * productPriceThree;
+                  setAmount3(e.target.value);
+                  setTotal3(currentTotal);
+                  const saleTotal = currentTotal + total + total2;
+                  setTotalSale(saleTotal);
+
+                  const priceTotalIva1 = total * iva;
+                  const priceTotalIva2 = total2 * iva2;
+                  const priceTotalIva3 = currentTotal * iva3;
+                  const totalIva =
+                    priceTotalIva1 + priceTotalIva2 + priceTotalIva3;
+                  setTotalIva(totalIva);
+
+                  const sumaIva = parseInt(saleTotal) + parseInt(totalIva);
+                  setSales(sumaIva);
+                }}
+                value={amount3}
+              />
             </Form.Group>
           </Col>
           <Col>
             <Form.Group className="mb-3 w-100">
-              <Form.Control type="text" />
+              <Form.Control
+                disabled
+                type="text"
+                onChange={(e) => {
+                  setTotal3(e.target.value);
+                }}
+                value={total3}
+              />
             </Form.Group>
           </Col>
         </Row>
@@ -176,7 +519,15 @@ const Sales = () => {
             <Form.Label>Total Venta</Form.Label>
           </Col>
           <Col xs="2">
-            <Form.Control className="mb-3 w-100" type="text" />
+            <Form.Control
+              disabled
+              className="mb-3 w-100"
+              type="text"
+              onChange={(e) => {
+                setTotalSale(e.target.value);
+              }}
+              value={totalSale}
+            />
           </Col>
         </Row>
         <Row>
@@ -185,7 +536,15 @@ const Sales = () => {
             <Form.Label>Total IVA</Form.Label>
           </Col>
           <Col xs="2">
-            <Form.Control className="mb-3 w-100" type="text" />
+            <Form.Control
+              disabled
+              className="mb-3 w-100"
+              type="text"
+              onChange={(e) => {
+                setTotalIva(e.target.value);
+              }}
+              value={totalIva}
+            />
           </Col>
         </Row>
         <Row>
@@ -194,7 +553,15 @@ const Sales = () => {
             <Form.Label>Total con IVA</Form.Label>
           </Col>
           <Col xs="2">
-            <Form.Control className="mb-3 w-100" type="text" />
+            <Form.Control
+              disabled
+              className="mb-3 w-100"
+              type="text"
+              onChange={(e) => {
+                setSales(e.target.value);
+              }}
+              value={sales}
+            />
           </Col>
         </Row>
         <Row>
@@ -212,6 +579,9 @@ const Sales = () => {
             <Button
               style={{ width: "200px", marginLeft: 40, marginTop: 10 }}
               type="text"
+              onClick={() => {
+                saveSale();
+              }}
             >
               Confirmar
             </Button>
